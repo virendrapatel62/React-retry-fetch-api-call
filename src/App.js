5df41f7d5e5bddf3efbccb80c7b5281379eb1187
virendrapatel62 <patelvirendra62@gmail.com>
2022-06-15 17:44:32 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.css';
 
 export default function App() {
+  const [retryCount, setRetryCount] = React.useState(0);
+  const [data, setData] = React.useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw response;
+      })
+      .then((data) => setData(data))
+      .catch((error) => retryCount < 3 && setRetryCount(retryCount + 1));
+  }, [retryCount]);
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
